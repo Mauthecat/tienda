4cbe97ef960f7d2162ad08bdb5eb88faad638abc
Ces <ces.hormazabal.g@gmail.com>
2026-02-06 00:16:16 -0300

    Fix

--- proyecto/proyecto/settings.py
+++ proyecto/proyecto/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "https://policromica.vercel.app",
