6909e410a4e73bbd1d41086e66e6b648b9ffab53
Ces <ces.hormazabal.g@gmail.com>
2026-02-07 13:40:59 -0300

    Fix: Limpieza total de dependencias y reordenamiento 2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    dedupe: ['react', 'react-dom'], // <--- ESTO ES LA CLAVE 1
+    alias: {
+      // <--- ESTO ES LA CLAVE 2: Forzamos a que apunte a SU propia carpeta node_modules
+      react: path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+    },
+  },
 })
